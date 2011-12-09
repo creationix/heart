0a7dc825d755a5006152f3edb9345eec6540593a
Tim Caswell <tim@creationix.com>
2011-12-08 23:36:39 -0600

    Add version command

--- heart.lua
+++ heart.lua
@@ -4,6 +4,7 @@ if command == "install" then return require('./install.lua')
 elseif command == "bin" then return require('./bin.lua')
 elseif command == "link" then return require('./link.lua')
 elseif command == "ls" then return require('./ls.lua')
+elseif command == "-v" or command == "version" then print(require('./package').version)
 else return require('./usage.lua') end
 
 
