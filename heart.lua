ebd0cc77868357d3bb4d5a9306f3c1095250d33f
Tim Caswell <tim@creationix.com>
2011-12-08 20:39:51 -0600

    Start to parse options

--- heart.lua
+++ heart.lua
@@ -1,3 +1,26 @@
 print("Heart Package Manager")
 
-p(process.argv)
+local command = process.argv[1]
+if command == "install" then
+  print("TODO: implement install")
+  return
+
+else
+  print[[
+Usage: heart <command>
+
+where <command> is one of:
+
+    bin      heart bin
+             heart bin -g
+             (just prints the bin folder)
+
+    install
+
+    list
+
+    help
+]]
+  return
+end
+
